4ac0c858fb965cb87d32b00d3ca9fac1149b2fd5
Zak Richardson <zakrc.richardson@gmail.com>
2017-05-30 16:10:33 +0100

    not certain on this but I feel this shouldn't test whether it can read the json but that it calls the method that does, so I mocked the value from read

--- spec/analyser_spec.rb
+++ spec/analyser_spec.rb
@@ -2,9 +2,31 @@ require 'analyser'
 
 describe Analyser do
 
+  json = ({
+    "videos"=>[
+      {
+        "title"=>"Test Video",
+        "views"=>401,
+        "link"=>"dQw4w9WgXcQ", "thumbnail"=>"https://i.ytimg.com/vi/gTUi5iUqQ_I/mqdefault.jpg",
+        "published_at"=>"2017-01-10T12:00:30.000Z",
+        "likes"=>25,
+        "dislikes"=>0
+      },
+      {
+        "title"=>"Better Video",
+        "views"=>93216,
+        "link"=>"dQw4w9WgXcQ", "thumbnail"=>"https://i.ytimg.com/vi/3ihrNAaBZU8/mqdefault.jpg",
+        "published_at"=>"2017-02-01T18:00:00.000Z",
+        "likes"=>5000,
+        "dislikes"=>30
+      }
+    ]
+  })
+
   describe "#parse_json" do
     context "when provided a file path" do
       it "will receive the formatted json from a parser" do
+        allow_any_instance_of(Parser).to receive(:read).with('test.json') { json }
         expect(subject.parse_json("test.json")["videos"].size).to eq 2
         expect(subject.parse_json("test.json")["videos"][0]["title"]).to eq "Test Video"
       end
