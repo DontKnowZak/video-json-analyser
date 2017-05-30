3c52654285d5fcf1effbf91e984ec424ddc4d20d
Zak Richardson <zakrc.richardson@gmail.com>
2017-05-30 16:03:10 +0100

    reference parser method in analyser

--- lib/analyser.rb
+++ lib/analyser.rb
@@ -3,8 +3,7 @@ require 'parser'
 class Analyser
 
   def parse_json(file_path = ARGV[0])
-    file = File.read(file_path)
-    data = JSON.parse(file)
+    data = @parser.read(file_path)
   end
 
 end
