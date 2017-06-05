a5960fd9df1ad7ce577c771d40bded0b9e42cd61
Zak Richardson <zakrc.richardson@gmail.com>
2017-06-05 10:24:31 +0100

    all tests working again

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,9 +5,9 @@ describe Parser do
   describe '#read' do
     context 'when passed a json file path' do
       it 'opens the file and places them in a hash' do
-        json_result = subject.read('test.json')
-        expect(json_result["videos"].size).to eq 2
-        expect(json_result["videos"][0]["title"]).to eq "Test Video"
+        videos = subject.read('test.json')
+        expect(videos.size).to eq 2
+        expect(videos[0].title).to eq "Test Video"
       end
     end
   end
